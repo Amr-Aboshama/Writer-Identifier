d6605d1276385d8a64d324aa5ab8392ce01aaabf
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-01-17 08:36:19 +0200

    Finalizing Training Module

--- trainingModule.py
+++ trainingModule.py
@@ -1,36 +1,37 @@
-import numpy as np
+from featuresModule import *
+from preprocessingModule import *
+from sklearn import svm
 
-###################--------------GET TRAINING DATA------------------#####################
 # This function returns training features and corresponding labels of all test case
-def getTrainingData(trainingFormIDs, trainingLabels, yTrain, formsFeaturesVectors):
+def getFeaturesAndLables(images, labels, featuresCount):
     
-    for trainingIndex, formID in enumerate(trainingFormIDs):
-        formsFeaturesVectors, labels= getFeaturesAndLabels(formID, trainingLabels[trainingIndex], formsFeaturesVectors, yTrain)
+    xTrain = np.empty((0, featuresCount))
+    yTrain = np.empty((0,0))
+    for trainingIndex in range(0, len(images)):
 
-    xTrain = np.array(formsFeaturesVectors)
-    yTrain = np.array(yTrain)
+        extractedLines = preprocessImage(images[trainingIndex])
+        featuresVectors = np.array(getFeatures(extractedLines, featuresCount))
+        xTrain = np.vstack((xTrain, featuresVectors))
+        yTrain = np.append(yTrain, np.full((featuresVectors.shape[0], 1), labels[trainingIndex]))
     
     return xTrain, yTrain
 
-# This function is used to return test features vectors
-def getTestFeatures(filename, formsFeaturesVectors):
-    print("current image is ", filename)
-    extractedLines,gray = preprocessModule(filename)
+# # This function is used to return test features vectors
+# def getTestFeatures(image, formsFeaturesVectors):
+#     extractedLines = preprocessImage(image)
+#     featuresVectors = np.array(getFeatures(extractedLines))
+#     # formsFeaturesVectors = np.vstack((formsFeaturesVectors, featuresVectors))
+#     return featuresVectors
 
-    featuresVectors = np.array(getFeatures(extractedLines,gray))
-        
-    formsFeaturesVectors = np.vstack((formsFeaturesVectors, featuresVectors))
-            
-    return np.array(formsFeaturesVectors)
-
-##############----------------------MODEL-------------------#############
 # This function applies SVM classifier on test features given training set
 def SVM(xTrain, yTrain, xTest):
-    
+    # print('Training SVM Model...')
     clf = svm.SVC(gamma='scale')
     clf.fit(xTrain, yTrain)
+    # print('Finished Training SVM Model...')
+    # print('Predecting Test Results...')
     predictions = clf.predict(xTest)
-
+    # print('Finished Predecting Test Results...')
     uniquePredictions, uniquePredictionsCount = np.unique(predictions, return_counts=True)
     classification = uniquePredictions[np.argmax(uniquePredictionsCount)]
-    return classification, predictions
+    return int(classification)
