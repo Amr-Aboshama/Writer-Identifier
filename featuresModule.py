cd4d2f58ce0720bc5e1ef8f4c577062f0343c50d
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-01-17 08:35:41 +0200

    Add Features Module

--- featuresModule.py
+++ featuresModule.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage import feature
+from preprocessingModule import *
 
 def LocalBinaryPatternHistogram(numPoints,radius,image):
 
@@ -7,38 +8,28 @@ def LocalBinaryPatternHistogram(numPoints,radius,image):
     (hist, _) = np.histogram(lbp.ravel(),bins=np.arange(0, numPoints + 3),range=(0,numPoints + 2))
     # normalize the histogram
     hist = hist.astype("float")
-    hist /= (hist.sum())
+    histSum = hist.sum()
+    if histSum != 0:         # Divide if has value
+        hist /= histSum
     # return the histogram of Local Binary Patterns
     return hist
 
 #extract Features from each line
-def getFeatures(extractedLines,extractedLines_gray):
+def getFeatures(extractedLines, featuresCount):
     interword=[]
     hist=[]
-    for i,j in zip(extractedLines,extractedLines_gray):
+    for img in extractedLines:
         
-        hist.append(LocalBinaryPatternHistogram(8,2,i))
+        hist.append(LocalBinaryPatternHistogram(8,2,img))
     
     return hist
     
 # This function is used to return features vectors and their labels
-def getFeaturesAndLabels(filename, labelVal, formsFeaturesVectors, labels):
-    
-    print("current filename is ", filename)
-   
-    extractedLines,gray = preprocessModule(filename)
-    
-    featuresVectors = np.array(getFeatures(extractedLines,gray))  
-    #featuresVectors=featuresVectors.reshape(1,-1)
-    
-    print(featuresVectors.shape)
-    #print(featuresVectors)
-    
-    formsFeaturesVectors = np.vstack((formsFeaturesVectors, featuresVectors)) 
-    
-    for i in range(featuresVectors.shape[0]):
-        labels.append(labelVal)
-
-    return np.array(formsFeaturesVectors), labels
-
-    
+# def getFeaturesAndLabels(image):
+#     extractedLines = preprocessImage(image)
+#     featuresVectors = np.array(getFeatures(extractedLines))
+#     print(featuresVectors.shape)
+#     formsFeaturesVectors = np.vstack((formsFeaturesVectors, featuresVectors))
+#     for i in range(featuresVectors.shape[0]):
+#         labels.append(labelVal)
+#     return featuresVectors
