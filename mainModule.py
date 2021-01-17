1a3b392125da8fda1d6ab2ecc4a82d303fbd4b4e
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-01-17 08:36:49 +0200

    Finalizing Main Module

--- mainModule.py
+++ mainModule.py
@@ -1,21 +1,29 @@
 import os
+from sampleModule import *
 
 featuresCount = 10 
-dataPath = 'data'
+dataPath = 'data2'
 
 dirs = os.listdir(dataPath)
 
 timeFile = open(f'{dataPath}/time.txt', 'w')
 resultFile = open(f'{dataPath}/result.txt', 'w')
 
-total = len(dirs)
+total = 0
 pos = 0
+time = 0
 
 for dir in dirs:
-    t, r, sucess = trainAndTestSample(f'{dataPath}/{dir}', featuresCount)
+    if not os.path.isdir(os.path.join(dataPath, dir)):
+        continue
+
+    total += 1
+    t, r, sucess = trainAndTestSample(f'{dataPath}\\{dir}', featuresCount)
     pos += sucess
-    timeFile.write(t + '\n')
-    resultFile.write(r + '\n')
+    time += t
+    timeFile.write(f'{t}\n')
+    resultFile.write(f'{r}\n')
     print('Sample ', dir, ': ', sucess)
 
 print('Accuracy = ', pos*100/total, '%')
+print('Total Time = ', time, ' seconds')
