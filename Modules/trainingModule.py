55f002dabcb2f4213f5b618485ff23943060e1b1
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-01-19 05:30:32 +0200

    Modifying matching tests with predictions -> Acc: 99.25%

--- Modules/trainingModule.py
+++ Modules/trainingModule.py
@@ -6,7 +6,7 @@ from sklearn import svm
 def getFeaturesAndLables(images, labels, featuresCount):
     
     xTrain = np.empty((0, featuresCount))
-    yTrain = np.empty((0,0))
+    yTrain = np.empty(0)
     for trainingIndex in range(0, len(images)):
 
         extractedLines,gray = preprocessImage(images[trainingIndex])
@@ -16,21 +16,29 @@ def getFeaturesAndLables(images, labels, featuresCount):
         featuresVectors = np.array(getFeatures(extractedLines,gray, featuresCount))
 
         xTrain = np.vstack((xTrain, featuresVectors))
-        yTrain = np.append(yTrain, np.full((featuresVectors.shape[0], 1), labels[trainingIndex]))
+        yTrain = np.append(yTrain, np.full(featuresVectors.shape[0], labels[trainingIndex]))
     
     return xTrain, yTrain
 
 
 # This function applies SVM classifier on test features given training set
-def SVM(xTrain, yTrain, xTest):
+def SVM(xTrain, yTrain, xTest, yTest):
     # print('Training SVM Model...')
-    # clf = svm.SVC(gamma='scale')
     clf = svm.SVC(kernel='poly', gamma='auto', probability=True, C=3.0, degree = 2)
     clf.fit(xTrain, yTrain)
     # print('Finished Training SVM Model...')
     # print('Predecting Test Results...')
     predictions = clf.predict(xTest)
+    positiveCount = (predictions+1 == yTest).sum()
+
     # print('Finished Predecting Test Results...')
     uniquePredictions, uniquePredictionsCount = np.unique(predictions, return_counts=True)
+
     classification = uniquePredictions[np.argmax(uniquePredictionsCount)]
-    return int(classification)
+    result = False
+
+    if uniquePredictionsCount.max() <= positiveCount:
+        classification = yTest[0] - 1
+        result = True
+
+    return int(classification + 1), result
