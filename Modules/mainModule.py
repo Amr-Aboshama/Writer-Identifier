4556d074df8f7d86ee319f2375abe07d09462fa1
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-01-19 05:28:25 +0200

    Modifying positive naming in mainModule

--- Modules/mainModule.py
+++ Modules/mainModule.py
@@ -16,14 +16,14 @@ time = 0
 for dir in dirs:
     if not os.path.isdir(os.path.join(dataPath, dir)):
         continue
-
+    
     total += 1
-    t, r, sucess = trainAndTestSample(f'{dataPath}\\{dir}', featuresCount)
-    pos += sucess
+    t, r, positive = trainAndTestSample(f'{dataPath}\\{dir}', featuresCount)
+    pos += positive
     time += t
     timeFile.write(f'{t}\n')
     resultFile.write(f'{r}\n')
-    print('Sample ', dir, ': ', sucess)
+    print('Sample ', dir, ': ', positive)
 
 print('Accuracy = ', pos*100/total, '%')
 print('Total Time = ', time, ' seconds')
