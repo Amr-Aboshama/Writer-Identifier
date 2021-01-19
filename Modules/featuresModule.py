436fc381880f36aa6c8680fb2d63f6d9d084b25d
TMai-mostafa99 <47695218+TMai-mostafa99@users.noreply.github.com>
2021-01-19 19:20:56 +0200

    update comments on featureModule

--- Modules/featuresModule.py
+++ Modules/featuresModule.py
@@ -5,14 +5,16 @@ import cv2
 
 def LocalBinaryPatternHistogram(image,mask):
     hist = np.zeros(256)
-    #mask=255-mask
+    
     lbp = np.uint8(feature.local_binary_pattern(image, 8,1))
-    #print(lbp)
-    # Calculate LBP histogram of only black pixels
-    hist = cv2.calcHist([lbp], [0], mask, [256], [0, 256], hist, True)
+    
+    # Calculate LBP histogram 
+    #(images: lbp,channel: grayscale,mask: calculate only text part , histsize ,histrange,hist)
+    hist = cv2.calcHist([lbp], [0], mask, [256], [0, 256], hist)
+    
+    #return 1D list
     hist = hist.ravel()
-    #ist /= np.sum(hist)
-  
+    
     return hist
 
 
@@ -21,7 +23,7 @@ def getFeatures(extractedLines,extractedLines_gray, featuresCount):
     for i,j in zip(extractedLines,extractedLines_gray):
         hist.append(LocalBinaryPatternHistogram(j,i))
 
-    #print(hist)
+    
     hist /=np.mean(hist)
     return hist
     
